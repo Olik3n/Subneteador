d9f97ff21a8e65fdf417fb0064a5c3cbdec8de91
Olik3n <146633822+Olik3n@users.noreply.github.com>
2025-03-12 15:05:03 -0400

    Add files via upload

---
+++ Subneteador.py
@@ -0,0 +1,13 @@
+
+📖 **Manual de Uso:**
+Este script calcula la subred óptima para una dirección IP dada y un número de hosts.
+🔹 Uso: python script.py <IP> <Cantidad de Hosts>
+🔹 Ejemplo: python script.py 192.168.1.10 50
+🔹 El script determinará la mejor máscara de subred, CIDR y el rango de direcciones.
+
+❌ Error: Debes ingresar una IP y la cantidad de hosts.
+Ejemplo: python script.py 192.168.1.10 50
+
+
+** Process exited - Return Code: 1 **
+Press Enter to exit terminal
